a47b550af75910954b4f250356b8c1163e74177e
Daniel Reyes Veas <danielreyesveas@gmail.com>
2020-06-08 12:03:52 -0400

    'deploy'

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,21 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
-
+import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
+const middlewares = [];
+const isDevelopment = (process.env.NODE_ENV === 'development');
+
+if (isDevelopment === 'development'){
+    middlewares.push(logger);
+}
+
 export const store = createStore(
-    rootReducer, 
+    rootReducer,     
     compose(
-        typeof window === 'object' &&
+        applyMiddleware(...middlewares),
+        typeof isDevelopment && window === 'object' &&
+
             typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ?
                 window.__REDUX_DEVTOOLS_EXTENSION__(): f => f
                 
